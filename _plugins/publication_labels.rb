cda27e4903dd30d80927f1787a119f67eb0019a3
Yen-Chieh Huang <lashhw81@gmail.com>
2026-08-19 13:24:42 +0800

    rb fix

--- _plugins/publication_labels.rb
+++ _plugins/publication_labels.rb
@@ -1,4 +1,4 @@
-Jekyll::Hooks.register [:pages, :documents], :post_render do |item|
+Jekyll::Hooks.register :pages, :post_render do |item|
   item.output = item.output.gsub(">DOI</a>", ">Paper</a>")
   item.output = item.output.gsub(">Abs</a>", ">Abstract</a>")
 end
